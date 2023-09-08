1581e86af2e462242722f23ad3dce7fec9683acf
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2023-09-08 12:55:48 +0000

    update

---
+++ components/tabs/discover.tsx
@@ -0,0 +1,9 @@
+export default function Discover() {
+    return (
+        <>
+            <div className="bg-blue-300">
+                Discover    
+            </div>        
+        </>
+    )
+}
---
+++ firebase/getDummyData.tsx
@@ -0,0 +1,77 @@
+
+
+// Dummy data for testing (replace this with your actual data structure)
+const contactsData = [
+    {
+      id: "1",
+      name: "John Doe",
+      email: "johndoe@example.com",
+    },
+    {
+      id: "2",
+      name: "Jane Smith",
+      email: "janesmith@example.com",
+    },
+    // Add more dummy data items as needed
+  ];
+const shopData = [
+    {
+      id: "1",
+      name: "Gucci Mane",
+      price: "499",
+    },
+    // Add more dummy data items as needed
+  ];
+const promoData = [
+    {
+      //
+    },
+  ];
+const discoverData = [
+    {
+      //
+    },
+  ];
+  
+  // Function to fetch dummy data for the "Contacts" tab (for testing)
+  export const getContactsData = async () => {
+    // Simulate an asynchronous operation (e.g., fetching from a database)
+    return new Promise<any[]>((resolve) => {
+      setTimeout(() => {
+        resolve(contactsData);
+      }, 1000); // Simulate a delay of 1 second
+    });
+  };
+  
+  // Function to fetch dummy data for the "Shop" tab (for testing)
+  export const getShopData = async () => {
+    // Simulate an asynchronous operation (e.g., fetching from a database)
+    return new Promise<any[]>((resolve) => {
+      setTimeout(() => {
+        resolve(shopData);
+      }, 1000); // Simulate a delay of 1 second
+    });
+  };
+  
+  // Function to fetch dummy data for the "Promo" tab (for testing)
+  export const getPromoData = async () => {
+    // Simulate an asynchronous operation (e.g., fetching from a database)
+    return new Promise<any[]>((resolve) => {
+      setTimeout(() => {
+        resolve(promoData);
+      }, 1000); // Simulate a delay of 1 second
+    });
+  };
+  
+  // Function to fetch dummy data for the "Discover" tab (for testing)
+  export const getDiscoverData = async () => {
+    // Simulate an asynchronous operation (e.g., fetching from a database)
+    return new Promise<any[]>((resolve) => {
+      setTimeout(() => {
+        resolve(discoverData);
+      }, 1000); // Simulate a delay of 1 second
+    });
+  };
+  
+  // ...
+  
